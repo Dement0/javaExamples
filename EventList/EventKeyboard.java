c69a626ee1a7a4c95d308a2c126bd5a82ef95027
Ilker Aslan <33685811+Dement0@users.noreply.github.com>
2019-01-11 13:16:52 +0100

    Create EventKeyboard.java
    Yet to define methods and utilities

---
+++ EventList/EventKeyboard.java
@@ -0,0 +1,39 @@
+public class EventKeyboard extends Event {
+	
+	// Fields
+	private int keyboardCode;
+	private int modificatorState;
+	
+	// Constructors
+	public EventKeyboard() {
+		super();
+		this(600, 0);
+	}
+	
+	public EventKeyboard(int eventId, String eventDescription, int keyboardCode, int modificatorState) {
+		super(eventId, eventDescription);
+		setKeyboardCode(keyboardCode);
+		setModificatorState(modificatorState);
+	}
+	
+	// Setters & Getters
+	public void setKeyboardCode(int keyboardCode) {
+		this.keyboardCode = keyboardCode;
+	}
+	
+	public void setModificatorState(int modificatorState) {
+		this.modificatorState = modificatorState;
+	}
+	
+	public int getKeyboardCode() {
+		return this.keyboardCode;
+	}
+	
+	public int getModificatorState() {
+		return this.modificatorState;
+	}
+	
+	// Methods
+	
+	// Utilities
+}
