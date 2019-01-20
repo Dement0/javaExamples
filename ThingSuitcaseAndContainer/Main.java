a7ed5a0a5376c127c11dd334a8b9cbabcfef5aca
Ilker Aslan <33685811+Dement0@users.noreply.github.com>
2019-01-20 15:18:58 +0100

    Create Main.java

---
+++ ThingSuitcaseAndContainer/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] Container) {
+        Container container = new Container(1000);
+        addSuitcasesFullOfBricks(container);
+        System.out.println(container);
+    }
+
+    public static void addSuitcasesFullOfBricks(Container container) {
+        // adding 100 suitcases with one brick in each
+        for(int i = 1; i <= 100; i++) {
+            Thing thing = new Thing("Brick", i);
+            Suitcase suitcase = new Suitcase(i);
+            suitcase.addThing(thing);
+            container.addSuitcase(suitcase);
+        }
+    }
+}
