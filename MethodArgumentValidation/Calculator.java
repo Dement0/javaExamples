edea1c5d4811260d8a075737b46065c2f01c60d4
Ilker Aslan <33685811+Dement0@users.noreply.github.com>
2019-09-02 23:03:16 +0200

    Create Calculator.java

---
+++ MethodArgumentValidation/Calculator.java
@@ -0,0 +1,26 @@
+package validation;
+
+public class Calculator {
+
+    public int multiplication(int fromInteger) {
+        
+        // Check if parameter is negative
+        if(fromInteger < 0) {
+            throw new IllegalArgumentException("Parameter should not be negative.");
+        }
+        int multiplication = 1;
+        for (int i = 1; i <= fromInteger; i++) {
+            multiplication *= i;
+        }
+
+        return multiplication;
+    }
+
+    public int binomialCoefficient(int setSize, int subsetSize) {
+
+        int numerator = multiplication(setSize);
+        int denominator = multiplication(subsetSize) * multiplication(setSize - subsetSize);
+
+        return numerator / denominator;
+    }
+}
