6591d481743cff999b401fe672b14fa78679430d
Ilker Aslan <33685811+Dement0@users.noreply.github.com>
2019-09-04 11:29:05 +0200

    Update Printer.java

--- Printer/Printer.java
+++ Printer/Printer.java
@@ -1,24 +1,28 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Printer {
 
     // Attributes
+    private File file;
     private String fileName;
     private Scanner scanner;
     
     // Constructor
     public Printer(String fileName) throws Exception {
         this.fileName = fileName;
+        this.file = new File(fileName);
     }
     
     // Methods
-    public void printLinesWhichContain(String word) {
-        
+    public void printLinesWhichContain(String word) throws FileNotFoundException {
+
         // Prints the file lines which contain the parameter word
         // If the argument is an empty string, print all of the file
         
         // Create a new scanner object to read the file
-        this.scanner = new Scanner(this.fileName);
+        this.scanner = new Scanner(this.file);
         
         if(word.isEmpty()) {
             while(this.scanner.hasNextLine()) {
@@ -28,8 +32,9 @@ public class Printer {
         } else {
             while(this.scanner.hasNextLine()) {
                 String tempWord = this.scanner.nextLine();
-                if(tempWord.equals(word)) {
-                    System.out.println(tempWord);
+                String tempLine = tempWord;
+                if(tempLine.contains(word)) {
+                    System.out.println(tempLine);
                 }
             }
         }
